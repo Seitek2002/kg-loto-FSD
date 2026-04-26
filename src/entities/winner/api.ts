f1368b75ffacf3e791ab4a1ca5233ad1e9060848
Seitek2002 <seitekbegaliev@gmail.com>
2026-04-26 17:10:48 +0600

    changes

--- src/entities/winner/api.ts
+++ src/entities/winner/api.ts
@@ -2,14 +2,17 @@ import { useQuery } from "@tanstack/react-query";
 
 import api from "@/shared/api/apiClient";
 
-// import { PaginatedResult, Winner } from './types';
+import { PaginatedResult, Winner } from "./types";
+
+interface WinnersResponse {
+  data: PaginatedResult<Winner>;
+}
 
 export const useWinners = () => {
   return useQuery({
     queryKey: ["winners"],
-    queryFn: async () => {
-      // Подставь правильный тип ответа согласно твоему бэкенду
-      const { data } = await api.get<any>("/winners/");
+    queryFn: async (): Promise<Winner[]> => {
+      const { data } = await api.get<WinnersResponse>("/winners/");
       return data?.data?.results || [];
     },
     staleTime: 1000 * 60 * 5,
